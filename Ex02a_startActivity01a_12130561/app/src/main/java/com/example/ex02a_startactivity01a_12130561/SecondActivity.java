ac7382caa3c0dfee4c36ad67425217fea877d748
shaodado <shaoshineshine@gamil.com>
2024-10-30 14:08:07 +0800

    備份

---
+++ Ex02a_startActivity01a_12130561/app/src/main/java/com/example/ex02a_startactivity01a_12130561/SecondActivity.java
@@ -0,0 +1,39 @@
+package com.example.ex02a_startactivity01a_12130561;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity
+implements View.OnClickListener
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button btnReturn =(Button)this.findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+        //this.startActivity(intent);
+        this.finish();
+
+    }
+}
---
+++ Ex02a_startActivity01a_12130561/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ex02a_startActivity01a_12130561"
+include(":app")
+ 
